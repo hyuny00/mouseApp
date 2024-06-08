b2c5e621e65515f70fef2f2226eb91aa6c769e18
hyun <10jqk1@kakao.com>
2024-06-08 12:27:13 +0900

    수정

--- run.py
+++ run.py
@@ -21,6 +21,40 @@ records = []
 # 파일 선택 여부를 추적하기 위한 변수
 file_loaded = False
 
+# 드래그 중인지 여부
+is_dragging = False
+
+def on_mouse_button_press(event):
+    global is_dragging
+    is_dragging = True
+
+
+def on_mouse_button_release(event):
+    global is_dragging, selected_color, t, wordCount
+    x, y = event.x_root, event.y_root  # Capture coordinates
+
+
+
+    if(len(records)==0):
+        messagebox.showwarning("경고", "먼저 데이터를 로드하세요.")
+        return
+
+    if records:
+        records.pop(0)
+
+    file_label.config(text=f"x: {event.x_root}, y: {event.y_root}")    
+
+    # 첫 번째 클릭 레코드 추가
+    records.insert(0, {"event": "click", "x": x, "y": y, "description": ""})
+
+    if records:
+        print("Records:")
+        for record in records:
+            print(record)
+    else:
+        print("Records are empty")
+
+
 def load_history():
     global file_loaded
     file_path = filedialog.askopenfilename(title="파일 선택", filetypes=[("json 파일", "*.json"), ("모든 파일", "*.*")])
@@ -62,12 +96,18 @@ root = tk.Tk()
 root.title("RUN")
 
 # 고정 크기 설정
-root.geometry("400x300")
+root.geometry("400x400")
 root.resizable(False, False)
 
 # 상단 프레임 (로드데이터 버튼과 파일명 표시)
 top_frame = tk.Frame(root)
 top_frame.pack(side=tk.TOP, pady=10)
+
+mouse_button = ttk.Button(top_frame, text="마우스")
+mouse_button.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=5, pady=5)
+mouse_button.bind("<ButtonPress-1>", on_mouse_button_press)
+mouse_button.bind("<ButtonRelease-1>", on_mouse_button_release)
+
 
 # 로드데이터 버튼
 load_button = tk.Button(top_frame, text="로드데이터", command=load_history)
@@ -77,12 +117,15 @@ load_button.pack(side=tk.LEFT, padx=5)
 file_label = tk.Label(top_frame, text="파일이 선택되지 않았습니다.")
 file_label.pack(side=tk.LEFT, padx=5)
 
+
+
+
 # 중앙 프레임 (시작 버튼과 중지 버튼)
 center_frame = tk.Frame(root)
 center_frame.pack(expand=True)
 
-# 버튼 크기 설정
-button_size = 180
+
+
 
 # 시작 버튼
 start_button = tk.Button(center_frame, text="시작", command=on_start_button, state=tk.DISABLED, width=10, height=5)
